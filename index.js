ec2a0f2e11236436b7b7376259b755858d5b33b1
Vulpie <jasonxs2001@gmail.com>
2026-03-18 19:38:08 -0400

    fix port for railway

--- index.js
+++ index.js
@@ -162,9 +162,11 @@ client.on('messageReactionRemove', async (reaction, user) => {
 });
 // ===== START SERVER FUNCTION =====
 function startServer() {
-  app.listen(3000, () => {
-    console.log("⚡ Webhook server live on port 3000");
-  });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`⚡ Server running on port ${PORT}`);
+});
 }
 // ===== LOGIN =====
 client.login(process.env.DISCORD_TOKEN);
