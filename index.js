58942d1760bf8bbba3ace70090ec159b5ef69a35
Vulpie <jasonxs2001@gmail.com>
2026-03-23 21:23:37 -0400

    im so tired of this

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ client.on('messageReactionRemove', async (reaction, user) => {
 function startServer() {
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`⚡ Server running on port ${PORT}`);
 });
 }
